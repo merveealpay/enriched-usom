aef3f01571d897640c85d0c84d13047327d4d799
mervealpay <merveealpay@gmail.com>
2024-03-31 01:49:04 +0300

    Improved WHOIS and DNS resolution

--- enriched_usom.py
+++ enriched_usom.py
@@ -2,6 +2,7 @@ import requests
 import whois
 import re
 import dns.resolver
+import datetime
 
 url = "https://usom.gov.tr/url-list.txt"
 
@@ -11,13 +12,16 @@ ioc_list = response.text.splitlines()
 
 output_file = "usom_enriched.txt"
 
+
 def find_similar_iocs(domain):
     try:
-        a_records = dns.resolver.resolve(domain, 'A')
+        resolver = dns.resolver.Resolver()
+        resolver.lifetime = 10
+        a_records = resolver.resolve(domain, 'A')
         ip_addresses = [record.to_text() for record in a_records]
 
         for ip_address in ip_addresses:
-            ptr_records = dns.resolver.resolve(ip_address, 'PTR')
+            ptr_records = resolver.resolve(ip_address, 'PTR')
             for ptr_record in ptr_records:
                 similar_domain = ptr_record.to_text()
                 if similar_domain != domain:
@@ -26,22 +30,55 @@ def find_similar_iocs(domain):
         print(f"DNS record not found: {domain}")
     except dns.resolver.NXDOMAIN:
         print(f"DNS record not found: {domain}")
+    except dns.resolver.Timeout:
+        print(f"DNS resolution timed out for domain: {domain}.")
+
 
 def perform_whois_analysis(domain):
     whois_info = whois.whois(domain)
+
+    if whois_info is None:
+        print(f"WHOIS information not available for domain: {domain}")
+        return
+
+    if all(value is None for value in whois_info.values()):
+        print(f"No WHOIS information available for domain: {domain}")
+        return
+
+    common_criteria = [
+        'registrant',
+        'name_servers',
+        'emails',
+        'address'
+    ]
+    for criteria in common_criteria:
+        if criteria in whois_info and whois_info[criteria] is not None:
+            for whois_entry in whois_info[criteria]:
+                for ioc in ioc_list:
+                    escaped_ioc = re.escape(ioc)
+                    if re.search(escaped_ioc, str(whois_entry), re.IGNORECASE):
+                        print(f"Similar WHOIS entry found for IOC: {ioc}")
 
     with open(output_file, "a") as file:
         file.write(f"Domain: {domain}\n")
         file.write(f"WHOIS Info: {whois_info}\n\n")
 
+def passive_dns_analysis(domain):
+    now = datetime.datetime.now()
+    delta = datetime.timedelta(days=7)
+    week_ago = now - delta
+
+
 for ioc in ioc_list:
-    domain = ioc.strip()
 
     try:
+        domain = ioc.strip()
         perform_whois_analysis(domain)
     except whois.parser.PywhoisError:
         print(f"WHOIS information couldn't be retrieved: {domain}")
 
     find_similar_iocs(domain)
+
+    passive_dns_analysis(domain)
 
 print("Analysis completed. Results have been saved to", output_file, "file.")
